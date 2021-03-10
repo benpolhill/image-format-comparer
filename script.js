0ba4b062d9090025efc32e1aaaa3dbe5153cca2d
Ben Polhill <benpolhill@gmail.com>
2021-03-10 22:03:30 +0000

    Initial commit! Horaaayyyyyyy!!!!

---
+++ script.js
@@ -0,0 +1,22 @@
+let fileName = {
+    quality: 'q60',
+    colour: 'rgb', 
+    blur: 'b0',
+    format: 'jpg'
+};
+console.log("init fileName:");
+console.log(fileName);
+
+document.addEventListener('click', el => {
+    console.log(el.target);
+    let value = el.target.id;
+    let name = el.target.name;
+    console.log(`${name} is ${value}!`);
+    let imgFile = 
+        `${name == 'quality' ? value : quality}_`+
+        `${name == 'colour' ? value : colour}_`+
+        `${name == 'blur' ? value : blur}.`+
+        `${name == 'format' ? value : format}`;
+    console.log(imgFile);
+    document.getElementById('main-img').setAttribute('src', `images/${imgFile}`);
+});
