1cc5f9dbb144f5c29900d8fd6919eb49c1630295
Ben Polhill <ben.polhill@tate.org.uk>
2021-03-11 12:04:56 +0000

    Get it all working

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
 const sizesJson = `./sizes.json`;
-
-fetch(sizesJson)
-    .then(response => response.json())
-    .then(data => console.log(data));
-
 let fileNameObj = {
-    quality: 'q60',
-    colour: 'rgb', 
-    blur: 'b0',
-    format: 'jpg'
+    quality: 'q60', colour: 'rgb', blur: 'b0', format: 'jpg'
 };
-console.log("init fileNameObj:");
-console.log(fileNameObj);
+
+async function getSizesJson(file) {
+    const response = await fetch(sizesJson);
+    const data = await response.json();
+    const sizeMsg = document.querySelector("#main-img-size .value");
+    Object.keys(data).forEach(k => {
+        if (file === k) {
+            console.log(`File ${k} is ${data[k]} bytes`);
+            sizeMsg.innerHTML = `${Math.round(data[k] / 1000)} <span>KB</span>`;
+        }
+    })
+}
 
 document.addEventListener('click', el => {
     if (el.target.type !== "radio") return;
-    console.log(el.target);
     let key = el.target.name;
     let value = el.target.value;
-    console.log(`${key} is ${value}!`);
-    // If the clicked key is in the object, change its value to value
     Object.keys(fileNameObj).forEach(k => {
-        console.log(`Key: ${k}`);
         k === key ? fileNameObj[k] = value : null;
     });
-    console.log('fileNameObj is now:');
-    console.log(fileNameObj);
 
     let imgFile = 
         `${key == 'quality' ? value : fileNameObj.quality}_`+
         `${key == 'colour' ? value : fileNameObj.colour}_`+
         `${key == 'blur' ? value : fileNameObj.blur}.`+
         `${key == 'format' ? value : fileNameObj.format}`;
-    console.log(imgFile);
+   
     document.getElementById('main-img').setAttribute('src', `images/${imgFile}`);
+
+    getSizesJson(imgFile);
 });
