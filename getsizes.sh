e4d27963a1892eac127957bc7d310c13abfb4ea2
Ben Polhill <ben.polhill@tate.org.uk>
2021-03-11 02:07:32 +0000

    Oops

--- getsizes.sh
+++ getsizes.sh
@@ -2,7 +2,7 @@
 rm sizes.json
 echo "{" >> sizes.json
 for f in images/*; do
-    size=$(stat -c%s: sh ./getsizes.hsens- "$f")
+    size=$(stat -c%s "$f")
     echo "'$f': '$size'," >> sizes.json
 done
 echo "}" >> sizes.json
