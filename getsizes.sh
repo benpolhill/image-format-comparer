cff0212715c884f0c2184129704ce99fda2c127a
Ben Polhill <benpolhill@gmail.com>
2021-10-13 14:05:17 +0100

    Update getSizes script & sizes.json

--- getsizes.sh
+++ getsizes.sh
@@ -3,10 +3,10 @@ rm sizes.json
 echo "{" >> sizes.json
 for f in images/*; do
     # Linux:
-    size=$(stat -c%s "$f")
+    #size=$(stat -c%s "$f")
     # Mac OS:
-    #size=$(stat -f%z "$f")
-    echo "'$f': '$size'," >> sizes.json
+    size=$(stat -f%z "$f")
+    echo "\"$f\": \"$size\"," >> sizes.json
 done
 echo "}" >> sizes.json
 
